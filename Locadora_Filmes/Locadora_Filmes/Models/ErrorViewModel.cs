3b1558c5c328ce30f2c61b42fa2321ef6cfa5ae8
Mauricio-HNS <90620341+Mauricio-HNS@users.noreply.github.com>
2022-08-20 18:58:26 -0300

    Commit-01
    Instalação do projeto.

---
+++ Locadora_Filmes/Locadora_Filmes/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Locadora_Filmes.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
